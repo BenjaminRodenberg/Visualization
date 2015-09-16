a1f064328d93ae2f43287f687a0e6cd32c0aaa08
Benjamin Rüth <benjamin.rueth@tum.de>
2015-09-16 18:01:07 +0200

    added minimal example

---
+++ MinimalInteractive/README.md
@@ -0,0 +1,17 @@
+# Minimal App
+This app is just a minimal example with interaction.
+
+## Running
+Enter 
+```
+$ bokeh-server --script minimal_app.py
+```
+in bash to run the app. Then enter
+```
+http://localhost:5006/bokeh/minimal
+```
+in your browser to use the app in it.
+
+##ToDos
+- [ ] Try something with embedding into html
+- [ ] Try to publish this to the internet
---
+++ MinimalInteractive/minimal_app.py
@@ -0,0 +1,142 @@
+__author__ = 'benjamin'
+
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 11 22:04:14 2015
+
+@author: benjamin
+"""
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+from bokeh.models.widgets import HBox, Slider, RadioButtonGroup, VBoxForm, Dropdown
+from bokeh.models import Plot, ColumnDataSource
+from bokeh.properties import Instance
+from bokeh.plotting import figure
+from bokeh.server.app import bokeh_app
+from bokeh.server.utils.plugins import object_page
+
+import numpy as np
+
+
+class MinApp(HBox):
+    # ==============================================================================
+    # Only bokeh quantities for layout, data, controls... go here!
+    # ==============================================================================
+    extra_generated_classes = [["MinApp", "MinApp", "HBox"]]
+
+    # layout
+    controls = Instance(VBoxForm)
+
+    # controllable values
+    slope = Instance(Slider)
+
+    # plot
+    plot = Instance(Plot)
+
+    # data
+    source = Instance(ColumnDataSource)
+
+    @classmethod
+    def create(cls):
+        # ==============================================================================
+        # creates initial layout and data
+        # ==============================================================================
+        obj = cls()
+
+        # default values
+        m = 1
+
+        # initialize data source
+        obj.source = ColumnDataSource(data=dict(x=[], y=[]))
+
+        # initialize controls
+        # slider controlling stepsize of the solver
+        obj.slope = Slider(
+            title="slope", name='slope',
+            value=m, start=-1, end=1, step=.05
+        )
+
+        # initialize plot
+        toolset = "crosshair,pan,reset,resize,wheel_zoom,box_zoom"
+        # Generate a figure container
+        plot = figure(title_text_font_size="12pt",
+                      plot_height=400,
+                      plot_width=400,
+                      tools=toolset,
+                      # title=obj.text.value,
+                      title="line",
+                      x_range=[0, 1],
+                      y_range=[-1, 1]
+                      )
+        # Plot the numerical solution by the x,t values in the source property
+        plot.line('x', 'y', source=obj.source)
+        obj.plot = plot
+
+        # calculate data
+        obj.update_data()
+
+        # lists all the controls in our app
+        obj.controls = VBoxForm(
+            children=[
+                obj.slope
+            ]
+        )
+
+        # make layout
+        obj.children.append(obj.controls)
+        obj.children.append(obj.plot)
+
+        # don't forget to return!
+        return obj
+
+    def setup_events(self):
+        # ==============================================================================
+        # Here we have to set up the event behaviour.
+        # ==============================================================================
+        # recursively searches the right level?
+        if not self.slope:
+            return
+
+        # event registration
+        self.slope.on_change('value', self, 'input_change')
+
+    def input_change(self, obj, attrname, old, new):
+        # ==============================================================================
+        # This function is called if input changes
+        # ==============================================================================
+        print "input changed!"
+        self.update_data()
+
+    def update_data(self):
+        # ==============================================================================
+        # Updated the data respective to input
+        # ==============================================================================
+
+        x = np.linspace(0,1)
+        y = np.empty(x.shape)
+        n = x.size
+        m = self.slope.value
+        t = 0
+
+        for i in range(n):
+            y[i] = m*x[i]+t
+
+        x=x.tolist()
+        y=y.tolist()
+
+        self.source.data = dict(x=x, y=y)
+
+        print "data was updated with parameters: m=" + str(m)
+
+
+@bokeh_app.route("/bokeh/minimal/")
+@object_page("minimal")
+def make_ode():
+    app = MinApp.create()
+    return app
+
+
+
