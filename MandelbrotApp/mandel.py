af78d7082bcc7ce6cb32c268060565a33e344671
Benjamin Rüth <benjamin.rueth@tum.de>
2016-03-18 12:07:33 +0100

    some cleaning up in static code, APP STILL NOT WORKING!

--- MandelbrotApp/mandel.py
+++ MandelbrotApp/mandel.py
@@ -6,8 +6,11 @@ import time
 
 def mandel(x0,y0,xw,yw,x_res,y_res, iterate_max, iteration_bound):
     # Calculate the mandelbrot sequence for the point c with start value z
-    @guvectorize(['void(float64[:,:], float64[:,:],float64[:,:],float64[:,:],float64[:,:])'],'(m,n),(m,n)->(m,n),(m,n),(m,n)')
-    def iterate_mandelbrot(c_re, c_im, it_count, z_re, z_im):
+    @guvectorize(['void(float64[:,:], float64[:,:],float64[:,:])'],'(m,n),(m,n)->(m,n)')
+    def iterate_mandelbrot(c_re, c_im, it_count):
+
+        apply_smoothening = True # trigger for applying smooth color algorithm or not
+
         m, n = c_re.shape
 
         for i in range(m):
@@ -24,13 +27,11 @@ def mandel(x0,y0,xw,yw,x_res,y_res, iterate_max, iteration_bound):
                     if (xx+yy) > iteration_bound:
                         count = it_n
                         break
-                z_re[i,j] = re
-                z_im[i,j] = im
 
-                if ( count < iterate_max ):
-                    log_zn = np.log( xx + yy ) / 2
-                    nu = np.log( log_zn / np.log(2) ) / np.log(2)
-                    count = count + 1 - nu
+                if count < iterate_max and apply_smoothening:
+                    log_zn = np.log( xx + yy ) * .5
+                    nu = np.log(log_zn / np.log(2)) / np.log(2)
+                    count += 1 - nu
 
                 it_count[i,j] = count
 
@@ -38,12 +39,9 @@ def mandel(x0,y0,xw,yw,x_res,y_res, iterate_max, iteration_bound):
 
     # Draw our image
     x, y = np.meshgrid(np.linspace(x0, x0 + xw, x_res,dtype=np.float64), np.linspace(y0, y0 + yw, y_res,dtype=np.float64))
-    z_re = np.zeros(x.shape,dtype=np.float64)
-    z_im = np.zeros(x.shape,dtype=np.float64)
     it_count = np.zeros(x.shape,dtype=np.float64)
-    print "calling iterate"
-    iterate_mandelbrot(x,y,it_count,z_re,z_im)
-
+    print "calling iterate_mandelbrot."
+    iterate_mandelbrot(x,y,it_count)
     print "elapsed time:"+str(time.clock()-t0)+" sec"
 
-    return x,y,it_count,z_re,z_im
+    return it_count
